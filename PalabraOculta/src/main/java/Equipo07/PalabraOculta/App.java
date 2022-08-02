938a02f96334ee38869b5fa31829c32c4dd3c142
FelipeGomezGarcia <feli.goga@gmail.com>
2022-08-02 09:08:39 +0200

    Interfaz grafica vacia

--- PalabraOculta/src/main/java/Equipo07/PalabraOculta/App.java
+++ PalabraOculta/src/main/java/Equipo07/PalabraOculta/App.java
@@ -1,13 +1,26 @@
 package Equipo07.PalabraOculta;
 
+import java.awt.EventQueue;
+
+import Equipo07.Interfaz.InterfazGrafica;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					InterfazGrafica frame = new InterfazGrafica();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
 }
