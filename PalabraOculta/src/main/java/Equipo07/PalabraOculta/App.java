f68258692f41dbfc1e10fd63da1a6261bdddca3d
Adrià Milan <adria4milan@gmail.com>
2022-08-02 09:03:56 +0200

    Initial PalabraOculta

---
+++ PalabraOculta/src/main/java/Equipo07/PalabraOculta/App.java
@@ -0,0 +1,13 @@
+package Equipo07.PalabraOculta;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
