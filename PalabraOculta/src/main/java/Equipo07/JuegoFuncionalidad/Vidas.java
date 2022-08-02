440f6b57ae0f6d698ed670e1dea0bd7937d6228f
Adrià Milan <adria4milan@gmail.com>
2022-08-02 10:21:19 +0200

    PalabraOculta JuegoFuncionalidad v1.1 rts branch

---
+++ PalabraOculta/src/main/java/Equipo07/JuegoFuncionalidad/Vidas.java
@@ -0,0 +1,22 @@
+package Equipo07.JuegoFuncionalidad;
+
+public class Vidas {
+
+	int numVidas;
+	
+	public Vidas() {
+		this.numVidas = 10;
+	}
+	
+	public Vidas(int vidas) {
+		this.numVidas = vidas;
+	}
+
+	/**
+	 * @return the numVidas
+	 */
+	public int getNumVidas() {
+		return numVidas;
+	}
+	
+}
